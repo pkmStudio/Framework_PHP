3b1a5aab3763cec4f4e8d675ba1b054042d889d7
pmkStudio Blog <pmkstudio77@gmail.com>
2023-03-10 13:45:44 +0300

    Внес корректировки по заметкам

--- app/controllers/AccountController.php
+++ app/controllers/AccountController.php
@@ -8,14 +8,6 @@ class AccountController extends Controller
 {
    public function loginAction()
    {
-      //! Если форма не пустая. Короче надо в отдельный метод вывести и проверки и т.д.
-      if (!empty($_POST)) {
-         $userlogin = trim(filter_var($_POST['userLogin'], FILTER_SANITIZE_SPECIAL_CHARS));
-         if (strlen($userlogin) > 3) {
-            $this->view->redirectAJAX('/account/register');
-         }
-         $this->view->message('Succsess', 200);
-      }
       //echo 'Страница входа';
       $this->view->render('Вход');
    }
